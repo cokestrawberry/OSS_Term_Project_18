ab8a820af70cff722f18d0dd5e52e96cce971764
Jimmy2849 <jee9893@gmail.com>
2023-06-04 15:29:56 +0900

    deal with branch name when HEAD is detached

--- flask-fileexplorer/setup.py
+++ flask-fileexplorer/setup.py
@@ -340,7 +340,13 @@ def filePage(var=""):
     if isgit:
         parsed_status = gitStatus_parsing()
         repo = git.Repo(var)
-        cur_branch = '<b>(' + repo.active_branch.name + ')</b>'
+        try:
+            cur_branch = '<b>(' + repo.active_branch.name + ')</b>'
+        except:
+            if(repo.head.is_detached):
+                cur_branch = '<b>(DETACHED_HEAD)</b>'
+            else:
+                cur_branch = ''
         return render_template('home.html', currentDir=var, favList=favList, default_view_css_1=default_view_css_1, default_view_css_2=default_view_css_2, view0_button=var1, view1_button=var2, currentDir_path=var_path, dir_dict=dir_dict, file_dict=file_dict, isgit=isgit, parsed_status=parsed_status, currentBranch_name = cur_branch)
     
     return render_template('home.html', currentDir=var, favList=favList, default_view_css_1=default_view_css_1, default_view_css_2=default_view_css_2, view0_button=var1, view1_button=var2, currentDir_path=var_path, dir_dict=dir_dict, file_dict=file_dict, isgit=isgit, parsed_status=None)
