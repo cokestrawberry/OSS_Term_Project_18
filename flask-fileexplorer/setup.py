90d5d88170ca1ed6273736419baffd74c7044b39
cokestrawberry <ljscj7325@naver.com>
2023-06-08 14:39:23 +0900

    타입 에러 수정 테스트

--- flask-fileexplorer/setup.py
+++ flask-fileexplorer/setup.py
@@ -357,7 +357,7 @@ def filePage(var=""):
         parsed_status = gitStatus_parsing()
         return render_template('home.html', currentDir=var, favList=favList, default_view_css_1=default_view_css_1, default_view_css_2=default_view_css_2, view0_button=var1, view1_button=var2, currentDir_path=var_path, dir_dict=dir_dict, file_dict=file_dict, isgit=isgit, parsed_status=parsed_status, is_branch=is_branch)
     
-    return render_template('home.html', currentDir=var, favList=favList, default_view_css_1=default_view_css_1, default_view_css_2=default_view_css_2, view0_button=var1, view1_button=var2, currentDir_path=var_path, dir_dict=dir_dict, file_dict=file_dict, isgit=isgit, parsed_status=None, is_branch=None)
+    return render_template('home.html', currentDir=var, favList=favList, default_view_css_1=default_view_css_1, default_view_css_2=default_view_css_2, view0_button=var1, view1_button=var2, currentDir_path=var_path, dir_dict=dir_dict, file_dict=file_dict, isgit=isgit, parsed_status=None, is_branch=is_branch)
     # ====================================
 
 @app.route('/', methods=['GET'])
